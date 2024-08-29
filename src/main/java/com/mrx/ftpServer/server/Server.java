b6c9c43f48e1b8c4fc9befe2ed817153fdda48ab
博麗靈夢 <1725338233@qq.com>
2024-08-30 07:10:15 +0800

    use try-with resources

--- src/main/java/com/mrx/ftpServer/server/Server.java
+++ src/main/java/com/mrx/ftpServer/server/Server.java
@@ -17,47 +17,34 @@ import java.net.Socket;
  */
 public class Server {
 
-    private ServerSocket welcomeSocket;
-
     private static final Logger logger = LoggerFactory.getLogger(Server.class);
 
-    public Server(String hostName, int controlPort) {
-        startServer(hostName, controlPort);
-    }
-
-    private void startServer(String hostName, int controlPort) {
-        try {
-            welcomeSocket = new ServerSocket(controlPort, 0, new InetSocketAddress(hostName, controlPort).getAddress());
+    @SuppressWarnings("InfiniteLoopStatement")
+    public static void startServer(String hostName, int controlPort) {
+        try (ServerSocket welcomeSocket = new ServerSocket(controlPort, 0, new InetSocketAddress(hostName, controlPort).getAddress())) {
+            logger.info("FTP Server started listening on port {}", controlPort);
+            int noOfThreads = 0;
+            while (true) {
+                try {
+                    TracerUtils.startTracer();
+                    Socket client = welcomeSocket.accept();
+                    // Port for incoming dataConnection (for passive mode) is the controlPort +
+                    // number of created threads + 1
+                    int dataPort = controlPort + noOfThreads++ + 1;
+                    // Create new worker thread for new connection
+                    Thread w = TracerUtils.startTracer(new Worker(client, dataPort));
+                    logger.info("New connection received. Worker was created.");
+                    w.start();
+                } catch (IOException e) {
+                    logger.warn("Exception encountered on accept", e);
+                } finally {
+                    TracerUtils.stopTracer();
+                }
+            }
         } catch (IOException e) {
             logger.error("Could not create server socket", e);
             System.exit(-1);
         }
-        logger.info("FTP Server started listening on port {}", controlPort);
-        int noOfThreads = 0;
-        while (true) {
-            try {
-                TracerUtils.startTracer();
-                Socket client = welcomeSocket.accept();
-                // Port for incoming dataConnection (for passive mode) is the controlPort +
-                // number of created threads + 1
-                int dataPort = controlPort + noOfThreads++ + 1;
-                // Create new worker thread for new connection
-                Thread w = TracerUtils.startTracer(new Worker(client, dataPort));
-                logger.info("New connection received. Worker was created.");
-                w.start();
-            } catch (IOException e) {
-                logger.warn("Exception encountered on accept", e);
-            } finally {
-                TracerUtils.stopTracer();
-            }
-        }
-//        try {
-//            welcomeSocket.close();
-//            logger.info("Server was stopped");
-//        } catch (IOException e) {
-//            logger.warn("Problem stopping server", e);
-//            System.exit(-1);
-//        }
     }
 
 }
