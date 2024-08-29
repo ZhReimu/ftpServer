5e4bfbd6d732e97516bd40cbac441fdda5e124f7
博麗靈夢 <1725338233@qq.com>
2024-08-29 19:12:39 +0800

    convert to maven project

---
+++ src/main/java/com/mrx/ftpServer/Main.java
@@ -0,0 +1,13 @@
+package com.mrx.ftpServer;
+
+/**
+ * @author Mr.X
+ * @since 2024/8/29 19:11
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        new Server();
+    }
+
+}
